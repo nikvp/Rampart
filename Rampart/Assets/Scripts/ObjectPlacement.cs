7b86fe1f28fcae96933b52aee1fd93a7efd4d5ec
BlueHillock <niklas.poljo@gmail.com>
2020-11-25 11:33:21 +0200

    grid ja objectplacerin eka versio, lisäsin testiscenen

---
+++ Rampart/Assets/Scripts/ObjectPlacement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacement : MonoBehaviour
+{
+    private Grid grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceObjectNear(hitInfo.point);
+            }
+        }
+    }
+    private void PlaceObjectNear(Vector3 nearPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(nearPoint);
+        //place Gameobject
+    }
+}
